ac41a629bb6f6f9f384ab61f051b9500f0f092c8
Zanul Harir <57970106+zanul11@users.noreply.github.com>
2024-12-09 11:50:49 +0800

    Update cetak.blade.php

--- resources/views/pages/absensi/laporan_absen/cetak.blade.php
+++ resources/views/pages/absensi/laporan_absen/cetak.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=21cm, initial-scale=1">
-    <meta name="description" content="Sistem Informasi Akademik Universitas Mataram">
-    <meta name="author" content="Universitas Mataram">
+    <meta name="description" content="Anumula">
+    <meta name="author" content="Anumula">
     <title>Laporan Rekap Absensi</title>
-    <link rel="stylesheet" href="{{asset('cetak/b.min.css')}}">
+    <!-- <link rel="stylesheet" href="{{asset('cetak/b.min.css')}}"> -->
     <link rel="stylesheet" href="{{asset('cetak/f.min.css')}}">
     <link rel="stylesheet" href="{{asset('cetak/style.css')}}">
 
@@ -16,28 +16,12 @@
     <link rel="apple-touch-icon-precomposed" href="{{asset('assets/img/logo.png')}}">
 
     <style>
-        @media print {
-            .garis {
-                background-color: black !important;
-                height: 3px;
-            }
-            table th, table td {
-                border:1px solid #fe1616;
-                padding:0.5em;
-            }
-        }
         
-        @media screen {
-            .garis {
-                background-color: black !important;
-                height: 3px;
-            }
-        }
     </style>
 </head>
 
 <body class="view mahasiswa halaman" onload="cetak()">
-    <div class="container-fluid cetak krs">
+    <div class="">
         <div class="row">
             {{-- <hr class="garis"> --}}
             <center style="margin-top: -10px;">
@@ -45,8 +29,8 @@
                 <b style="font-size: 10px;">PERIODE {{(Cache::has('dTgl'))?Cache::get('dTgl').' to '.Cache::get('sTgl'):date('d-m-Y')}}</b>
             </center>
             <br>
-            
-            <table class="table table-hover table-bordered">
+
+            <table class="table table-bordered">
                 <thead>
                     <tr>
                         <th style="width: 5%">#</th>
@@ -65,59 +49,75 @@
 
                 </thead>
                 <tbody>
-                @php
-                            $hari_kerja = 0;
-                            $kehadiran = 0;
-                            $telat = 0;
-                            $tanpa_keterangan = 0;
-                            $total_persen = 0;
-                        @endphp
+                    @php
+                    $hari_kerja = 0;
+                    $kehadiran = 0;
+                    $telat = 0;
+                    $tanpa_keterangan = 0;
+                    $total_persen = 0;
+                    @endphp
 
-                        @foreach ($jenis_izin as $izin) 
-                        @php
-                            ${$izin->name} = 0;
-                        @endphp
-                        @endforeach
-                    @foreach($data_absen as $data)
-                 
-                            @php
-                            $hari_kerja += $data['jam_kerja'];
-                            $kehadiran += $data['kehadiran'];
-                            $telat += $data['telat'];
-                            $tanpa_keterangan += $data['tanpa_keterangan'];
-                            $total_persen += ((($data['kehadiran']/$data['jam_kerja'])*100));
-                            @endphp
-                    <tr >
-                        <td align="center">{{$loop->iteration}}</td>
-                        <td align="center">{{$data['nip']}}</td>
-                        <td align="center">{{$data['nama']}}</td>
-                        <td align="center">{{$data['jam_kerja']}}</td>
-                        <td align="center">{{$data['kehadiran']}}</td>
-                        <td align="center">{{$data['telat']}}</td>
-                        <td align="center">{{$data['tanpa_keterangan']}}</td>
-                        @foreach ($jenis_izin as $izin) 
-                            <td align="center">{{$data[strtolower(str_replace(' ', '_', $izin->name))]}}</td>
-                        @endforeach
-                        <td align="center">{{$data['jam']}} Jam {{$data['menit']}} Menit</td>
-                        <td align="center">{{round(($data['kehadiran']/$data['jam_kerja'])*100,2)}}%</td>
-                    </tr>
+                    @foreach ($jenis_izin as $izin)
+                    @php
+                    ${$izin->name} = 0;
+                    @endphp
                     @endforeach
-                    <tr style="font-weight: 700">
-                                <td colspan="3" style="text-align: center">Total</td>
-                                <td>{{$hari_kerja}}</td>
-                                <td>{{$kehadiran}}</td>
-                                <td>{{$telat}}</td>
-                                <td>{{$tanpa_keterangan}}</td>
-                                @foreach ($jenis_izin as $izin) 
-                                <td>{{ ${$izin->name} }}</td>
-                                @endforeach
-                                <td colspan="2" style="text-align: center">{{round($total_persen/count($data_absen),2)}}%</td>
-                                
-                            </tr>
+                    @foreach($data_absen as $data)
+
+                    @php
+                    $hari_kerja += $data['jam_kerja'];
+                    $kehadiran += $data['kehadiran'];
+                    $telat += $data['telat'];
+                    $tanpa_keterangan += $data['tanpa_keterangan'];
+                    $total_persen += ((($data['kehadiran']/$data['jam_kerja'])*100));
+                    $persen_kehadiran = round(($data['kehadiran']/$data['jam_kerja'])*100,2);
+
+                    $persen_kehadiran = round(($data['kehadiran']/$data['jam_kerja'])*100,2);
+                    if($persen_kehadiran < 50){
+                        $warna='black' ;
+                        $warna_tulisan='white' ;
+                        } else if ($persen_kehadiran < 85) {
+                        $warna='red' ;
+                        $warna_tulisan='white' ;
+                        } else if ($persen_kehadiran < 90) {
+                        $warna='yellow' ;
+                        $warna_tulisan='black' ;
+                        }else {
+                        $warna='green' ;
+                        $warna_tulisan='white' ;
+                        }
+                        @endphp
+                        <tr style="background-color: {{$warna}}!important; -webkit-print-color-adjust: exact;">
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$loop->iteration}}</td>
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$data['nip']}}</td>
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$data['nama']}}</td>
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$data['jam_kerja']}}</td>
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$data['kehadiran']}}</td>
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$data['telat']}}</td>
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$data['tanpa_keterangan']}}</td>
+                        @foreach ($jenis_izin as $izin)
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$data[strtolower(str_replace(' ', '_', $izin->name))]}}</td>
+                        @endforeach
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$data['jam']}} Jam {{$data['menit']}} Menit</td>
+                        <td align="center" style="color: {{$warna_tulisan}};">{{$persen_kehadiran}}%</td>
+                        </tr>
+                        @endforeach
+                        <tr style="font-weight: 700; background-color: {{$warna}};">
+                            <td colspan="3" style="text-align: center">Total</td>
+                            <td>{{$hari_kerja}}</td>
+                            <td>{{$kehadiran}}</td>
+                            <td>{{$telat}}</td>
+                            <td>{{$tanpa_keterangan}}</td>
+                            @foreach ($jenis_izin as $izin)
+                            <td>{{ ${$izin->name} }}</td>
+                            @endforeach
+                            <td colspan="2" style="text-align: center">{{round($total_persen/count($data_absen),2)}}%</td>
+
+                        </tr>
                 </tbody>
                 <tfoot>
-                            
-                        </tfoot>
+
+                </tfoot>
             </table>
             <hr style="height:3px;  background-color:black">
             <div class="pull-left ttd">
@@ -128,7 +128,7 @@
                 <br>Mataram, {{date('d F Y')}}<br>
                 <br>Pembuat,
                 <br><br><br><br><br>
-                <span class="nama">{{ Auth::user()->name }}</span>
+                <span class="nama">{{ Auth::user()->nama }}</span>
             </div>
 
         </div>
@@ -143,7 +143,7 @@
 
     <script type="text/javascript">
         function cetak() {
-            window.print();
+            // window.print();
         };
     </script>
 
